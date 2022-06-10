7c46befb60334b4fa233d877f9569733412d57c6
doki42 <andras.szogi@gmail.com>
2022-06-10 19:12:51 +0200

    Compulsory tasks completed

--- static/main.js
+++ static/main.js
@@ -5,6 +5,7 @@ async function main(){
 
     const nextButton = document.getElementById('next');
     const prevButton = document.getElementById('previous');
+    const residentsModal = document.getElementById('residentsModal');
 
     nextButton.addEventListener('click', function(){
         url = nextButton.dataset.url;
@@ -18,12 +19,17 @@ async function main(){
         planets(url, buildTable);
         planets(url, setPrevButton);
         planets(url, setNextButton);
-        /*if (prevButton.dataset.url === "null"){
-            prevButton.classList.add("disabled");
-        } else {
-            prevButton.classList.remove('disabled')
-        }*/
-    })
+    });
+
+    residentsModal.addEventListener('show.bs.modal', event => {
+        const residents = event.relatedTarget.getAttribute('data-bs-residents');
+        const planetName = event.relatedTarget.getAttribute('data-bs-planet');
+        const residentsArr = residents.split(',');
+        const residentsModalTitle = residentsModal.querySelector('.modal-title')
+        residentsModalTitle.innerText = `Residents of ${planetName}`;
+        console.log(residentsArr)
+        showResidents(residentsArr);
+    });
 
 }
 
@@ -48,12 +54,7 @@ function setPrevButton(link)
     prevButton.dataset.url = link.previous;
 
 }
-/*function getPlanetTable(pageNum) {
-    swapiModule.getPlanets({page: pageNum}, function (data) {
-        buildTable(data.results);
-        console.log(data.next);
-    });
-}*/
+
 function clearTable() {
   const tableBody = document.querySelector('#table-body');
   while (tableBody.firstChild) tableBody.removeChild(tableBody.firstChild);
@@ -66,7 +67,7 @@ function buildTable(planets) {
   // Add the table content.
   planets.results.forEach(function(curr) {
     addLineToHTMLTable(curr.name, curr.diameter, curr.climate, curr.terrain, curr.surface_water, curr.population, curr.residents);
-  });
+    });
 }
 
 // Add a line to the HTML table
@@ -97,19 +98,70 @@ function addLineToHTMLTable(name, diameter, climate, terrain, surface_water, pop
     peopleCell.innerHTML = (population === "unknown") ? "unknown" : `${(population.toLocaleString())} people`;
 
     const residentsCell = newRow.insertCell();
-    residentsCell.innerHTML = (residents.length == 0) ? "No known residents" : showResidents(residents);
+    residentsCell.innerHTML = (residents.length == 0) ? "No known residents" : createResidentsButton(residents, name);
 
     const voteCell = newRow.insertCell();
     voteCell.innerHTML = '<button>Vote</button>';
+
 }
 
 function showResidents(residents)
 {
-   return residents.length
+    const tableBody = document.querySelector('#residentsTbody');
+    while (tableBody.firstChild) tableBody.removeChild(tableBody.firstChild);
+    for (let resident of residents){
+        planets(resident, buildModalTable)
+    }
+    function buildModalTable(people){
 
+    addLineToModalTable(people.name, people.height, people.mass, people.hair_color, people.skin_color, people.eye_color, people.birth_year, people.gender)
+
+    }
+
+    function addLineToModalTable(name, height, mass, hair, skin, eye, birth, gender) {
+      // Get the body of the table using the selector API
+      const tableBody = document.querySelector("#residentsTbody");
+
+      // Add a new row at the end of the table
+      const newRow = tableBody.insertRow();
+
+      // add  new cells to the row
+      const peopleNameCell = newRow.insertCell();
+      peopleNameCell.innerHTML = name;
+
+      const heightCell = newRow.insertCell();
+      heightCell.innerHTML = (height == "unknown") ? "unknown" : height;
+
+        const massCell = newRow.insertCell();
+        massCell.innerHTML = mass;
+
+        const hairCell = newRow.insertCell();
+        hairCell.innerHTML = hair;
+
+        const skinCell = newRow.insertCell();
+        skinCell.innerHTML = skin;
+
+        const eyeCell = newRow.insertCell();
+        eyeCell.innerHTML = eye;
+
+        const birthCell = newRow.insertCell();
+        birthCell.innerHTML = birth;
+
+        const genderCell = newRow.insertCell();
+        genderCell.innerHTML = gender;
+
+    }
 
 }
 
+function createResidentsButton(residents, planet)
+{
+    const residentsModal = new bootstrap.Modal ('#residentsModal');
+    return `<button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#residentsModal" 
+               data-bs-residents="${residents}" data-bs-planet="${planet}" name="residents">${residents.length} resident(s)</button>`
+}
+
+
+
 main();
 
-`${diameter.toLocaleString()} km`
